4ed8c8f85c7074d3112ad7fbbf752a4afb95ad0f
kongchun <kcrens@gmail.com>
2018-06-01 14:56:44 +0800

    公司数据清洗

--- robot/model/company.js
+++ robot/model/company.js
@@ -1,9 +1,10 @@
 'use strict';
 import mongodb from "mongodb";
 export default class company{
-    constructor({_id,url=null,company=null,addr=null,position=true,city=null,kd=null}) {
+    constructor({_id,url=null,jobIds = {},alias=null,company=null,addr=null,position=true,city=null,kd=null}) {
        this._id = _id?mongodb.ObjectId(_id):new mongodb.ObjectId();
        this.company = company;
+       this.alias = alias;
        this.addr = addr;
        this.count = count;
        this.position = position;
