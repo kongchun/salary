aea4434d84f31cdad6c04ed0f08235bfc84d8496
kongchun <kcrens@gmail.com>
2019-06-13 18:55:25 +0800

    修复bug

--- web/src/manage/tables.js
+++ web/src/manage/tables.js
@@ -2,29 +2,38 @@ $(function () {
     getCompanyDetail($('.table-salary-logo'), $('.table-salary-score'));
 });
 
-function getCompanyDetail(logos, scores, stop) {
+function getCompanyDetail(logos, scores, stop = false) {
     let companies = '';
     for (let logoImg of logos) {
-        companies += logoImg.getAttribute('company')+',';
+        companies += logoImg.getAttribute('company') + ',';
     }
     let param = companies.substring(0, companies.length - 1);
     $.post('/api/getCompanyLogoAndScore', { data: param }, function (data) {
-        if (!!data) {
-            for (let i = 0; i < data.length; i++) {
-                let item = data[i];
-                if(!item){
-                    return;
+        try {
+            if (data) {
+                for (let i = 0; i < data.length; i++) {
+                    let item = data[i];
+                    if (!item) {
+                        return;
+                    }
+                    if (!!item.logo) {
+                        logos[i].src = item.logo;
+                    }
+                    if (!!item.score) {
+                        scores[i].innerHTML = item.score;
+                    }
                 }
-                if (!!item.logo) {
-                    logos[i].src = item.logo;
-                }
-                if (!!item.score) {
-                    scores[i].innerHTML = item.score;
-                }
+
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            console.log(stop);
+            if (!stop) {
+                console.log(111);
+                getCompanyDetail($('.table-number-logo'), $('.table-number-score'), true);
             }
         }
-        if (!stop) {
-            getCompanyDetail($('.table-number-logo'), $('.table-number-score'), true);
-        }
+
     });
 }
